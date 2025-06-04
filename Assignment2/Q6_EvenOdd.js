0e83dce6f4d5f0807e8648636de8048784f8dbb4
nishint-successiveTech <nishint.goyal@successive.tech>
2025-06-04 15:12:09 +0530

    Added Return Statement

--- Assignment2/Q6_EvenOdd.js
+++ Assignment2/Q6_EvenOdd.js
@@ -1,14 +1,18 @@
-function evenOdd(x)
-{
-if(x%2==0)
-{
-    console.log("Number is Even: ",x);
-    return;
-}
-console.log("Number is Odd: ", x);
+function evenOdd(x){
+    if(x%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-for(let i=0;i<=50;i++)
+function evenOddUpto50()
 {
-    evenOdd(i);
+    for(let i=0;i<=50;i++){
+    let ans=evenOdd(i);
+    console.log(i+" is "+ans);
+    }
 }
+
+evenOddUpto50();
